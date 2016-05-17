05839024ad47c6eb2bd66f26b6105ecde96330b8
shizhouyong <zhouyong.shi@gmail.com>
2016-05-17 20:47:22 +0800

    commit by szy "SSM框架调试通过"

--- src/main/java/com/szy/mapper/UserMapper.java
+++ src/main/java/com/szy/mapper/UserMapper.java
@@ -1,16 +1,16 @@
 package com.szy.mapper;
 
 import com.szy.po.User;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 /**
  * Created by szy on 2016/5/14.
  */
 
-@Component("userMapper")
+@Repository
 public interface UserMapper {
 
-    public List<User> findUserList() throws Exception;
+    public List<User> findUsers() throws Exception;
 
 }
--- src/main/java/com/szy/service/impl/UserServiceImpl.java
+++ src/main/java/com/szy/service/impl/UserServiceImpl.java
@@ -4,20 +4,22 @@ import com.szy.mapper.UserMapper;
 import com.szy.po.User;
 import com.szy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by szy on 2016/5/14.
  */
+
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserMapper userMapper;
 
     public List<User> findUserList() throws Exception {
-        return userMapper.findUserList();
+        return userMapper.findUsers();
     }
 
 
